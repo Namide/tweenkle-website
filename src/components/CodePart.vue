38307f4113ded498dc545fbd21eb038178273195
Namide <damien.git@gmail.com>
2023-02-28 21:09:38 +0100

    Update syntax coloration tool

--- src/components/CodePart.vue
+++ src/components/CodePart.vue
@@ -1,44 +1,11 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
-import { getHighlighter } from "shiki";
-
-let raf: number;
-
-const props = withDefaults(defineProps<{ code: string; language?: string }>(), {
-  language: "js",
+withDefaults(defineProps<{ code: string; language?: string }>(), {
+  language: "javascript",
 });
-
-const colorizedCode = ref(`<pre><code>${props.code}</code></pre>`);
-
-if (!import.meta.env.SSR) {
-  const highlighter = getHighlighter({
-    theme: "monokai",
-    langs: ["javascript"],
-    paths: {
-      wasm: `${import.meta.env.BASE_URL}shiki`,
-      themes: `${import.meta.env.BASE_URL}shiki/themes`,
-      languages: `${import.meta.env.BASE_URL}shiki/languages`,
-    },
-  });
-  watch(
-    () => props.code,
-    () => {
-      cancelAnimationFrame(raf);
-      raf = requestAnimationFrame(async () => {
-        highlighter.then((highlighter) => {
-          colorizedCode.value = highlighter.codeToHtml(props.code, {
-            lang: props.language,
-          });
-        });
-      });
-    },
-    { immediate: true }
-  );
-}
 </script>
 
 <template>
-  <div v-html="colorizedCode"></div>
+  <highlightjs :language="language" :code="code" />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import routes from "./router";
+import "highlight.js/styles/monokai-sublime.css";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import hljsVuePlugin from "@highlightjs/vue-plugin/src/vue";
 
 import "./assets/main.css";
+
 import type { RouteRecordRaw } from "vue-router";
+
+hljs.registerLanguage("javascript", javascript);
 
 export const createApp = ViteSSG(
   App,
   { routes: routes as unknown as RouteRecordRaw[] },
   ({ app, router, routes, isClient, initialState }) => {
-    // app.use(VueHighlightJS);
+    app.use(hljsVuePlugin);
   }
 );
