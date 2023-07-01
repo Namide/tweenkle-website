263ac3bf9050141a82a8c21979b90782da52b80c
Namide <damien.git@gmail.com>
2023-07-02 00:15:18 +0200

    Add pixi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import useUpdateHead from "@/composable/useUpdateHead";
+import { defineAsyncComponent } from "vue";
+// const PixiExample = () => import("@/components/PixiExample.vue");
+
+const PixiExample = import.meta.env.SSR
+  ? null
+  : defineAsyncComponent(() => import("@/components/PixiExample.vue"));
 
 useUpdateHead();
 </script>
 
 <template>
-  <main class="flex-1"></main>
+  <main class="flex-1">
+    Test
+    <template v-if="PixiExample">
+      <PixiExample></PixiExample>
+    </template>
+  </main>
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,17 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    __DEV__: `import.meta.env.NODE_ENV !== "production"`,
-  },
-  // base: "/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    define: {
+      __DEV__: mode === "development" ? "true" : "false",
     },
-  },
+    // base: "/",
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
