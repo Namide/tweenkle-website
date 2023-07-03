e6d9c3f10b372d1503e98b4982fd31427839c02d
Namide <damien.git@gmail.com>
2023-07-03 22:54:33 +0200

    Init doc

--- src/views/guide/WhenUseTwon.vue
+++ src/views/guide/WhenUseTwon.vue
@@ -2,7 +2,18 @@
 
 <template>
   <div class="prose">
-    <h1 class="example-title">Guide</h1>
+    <h1 class="example-title">When use twon?</h1>
+    <p>
+      Twon is designed for pure Javascript animations. You can interpolate WebGL
+      animation with WebGL render library. You can use twon too for dynamic
+      animation like animations changed in real time by the user.
+    </p>
+    <p>
+      Avoid using twon or other tweening library for HTML precalculated
+      animations or transitions. For that it's better using CSS animatio or
+      transitions and combine transform and opacity modification. The framerate
+      and weight will be better.
+    </p>
   </div>
 </template>
 
--- src/views/guide/WhyTwon.vue
+++ src/views/guide/WhyTwon.vue
@@ -2,7 +2,29 @@
 
 <template>
   <div class="prose">
-    <h1 class="example-title">Guide</h1>
+    <h1 class="example-title">Why use twon?</h1>
+    <p>
+      Twon, like othe tween library, interpolate values with easing curves. With
+      twon you can manage 2 types of interpolations:
+    </p>
+    <ul>
+      <li><strong>Basic tween</strong> with interpolation between values</li>
+      <li>
+        <strong>Dynamic tween</strong> with adjusted curves as mofications are
+        made.
+      </li>
+    </ul>
+    <p>
+      You can interpolate 2 values, or
+      <strong>chain multiples tweens</strong> with multiple curves, or generate
+      a <strong>path</strong> of values. This path can be
+      <strong>smoothed</strong>.
+    </p>
+    <p>
+      You can use 31 standart easing functions or generate your easing with
+      cubic bezier values. If you need a more complex curve you can compose a
+      new easing by grouping easings.
+    </p>
   </div>
 </template>
 
